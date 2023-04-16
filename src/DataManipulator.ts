def6d6887bd74fd0bec2693cac56265847dd3732
Ye Yutong <e0425083@u.nus.edu>
2023-04-16 13:14:58 +0800

    completed

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,31 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_ABC: number,
+  price_DEF: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]) : Row {
+    const price_ABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+    const price_DEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+    const ratio = price_ABC / price_DEF;
+    const upper_bound = 1 + 0.1;
+    const lower_bound = 1 - 0.1;
+    return {
+      price_ABC,
+      price_DEF,
+      ratio,
+      timestamp: (serverResponds[0].timestamp > serverResponds[1].timestamp) ? serverResponds[0].timestamp : serverResponds[1].timestamp,
+      upper_bound,
+      lower_bound,
+      trigger_alert: (ratio > upper_bound || ratio < lower_bound) ? ratio : undefined,
+    };
   }
 }
